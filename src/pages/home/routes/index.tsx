e60791fbfff37d7efdccc7a00a5451aa28b6a217
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2024-06-26 09:52:55 +0200

    Update index.tsx

--- src/pages/home/routes/index.tsx
+++ src/pages/home/routes/index.tsx
@@ -1,7 +1,8 @@
 import HomePage from "..";
 import PrivateRouteGuard from "../../../guards/auth";
+import { RouteObject } from "react-router-dom";
 
-const HomePageRoutes = {
+const HomePageRoutes: RouteObject = {
   path: "/",
   element: (
     <PrivateRouteGuard>
