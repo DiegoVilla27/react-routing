990ca6a966e0639afcbd5aa94b059b6275a36577
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2024-06-26 09:53:26 +0200

    Update index.tsx

--- src/pages/about/routes/index.tsx
+++ src/pages/about/routes/index.tsx
@@ -1,7 +1,8 @@
 import AboutPage from "..";
 import PrivateRouteGuard from "../../../guards/auth";
+import { RouteObject } from "react-router-dom";
 
-const AboutPageRoutes = {
+const AboutPageRoutes: RouteObject = {
   path: "/about",
   element: (
     <PrivateRouteGuard>
