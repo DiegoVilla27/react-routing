459f9f4988794d81168e12e6abbe4dbe5f2bf536
Diego Villanueva <43555467+DiegoVilla27@users.noreply.github.com>
2024-06-26 09:50:57 +0200

    Update index.tsx

--- src/router/private/index.tsx
+++ src/router/private/index.tsx
@@ -1,7 +1,8 @@
 import AboutPageRoutes from "../../pages/about/routes";
 import HomePageRoutes from "../../pages/home/routes";
 import UsersPageRoutes from "../../pages/users/routes";
+import { RouteObject } from "react-router-dom";
 
-const RoutesPrivate = [HomePageRoutes, UsersPageRoutes, AboutPageRoutes];
+const RoutesPrivate: RouteObject[] = [HomePageRoutes, UsersPageRoutes, AboutPageRoutes];
 
 export default RoutesPrivate;
